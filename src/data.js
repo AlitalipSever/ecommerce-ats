16451382fcec74d41420a4f4f7abc60d76338c79
AlitalipSever <alitalipsever@gmail.com>
2022-01-18 20:54:47 +0100

    category added

--- src/data.js
+++ src/data.js
@@ -21,3 +21,31 @@ export const sliderItems = [
         bg: "fbf0f4",
     },
 ]
+
+export const categories = [
+    {
+        id: 1,
+        img:"https://www.carhartt-wip.com/binaries/content/gallery/pagetypes/looks/detail/women/2022ss/ss22-lookbook-women-11.jpg/ss22-lookbook-women-11.jpg/carhartt%3Alooksbigimage",
+        title:"Hats Galaxy"
+    },
+    {
+        id: 2,
+        img:"https://www.carhartt-wip.com/binaries/content/gallery/pagetypes/looks/detail/women/2022ss/ss22-lookbook-women-07.jpg/ss22-lookbook-women-07.jpg/carhartt%3Alooksbigimage",
+        title:"Jackets Love"
+    },
+    {
+        id: 3,
+        img:"https://www.carhartt-wip.com/binaries/content/gallery/pagetypes/looks/detail/women/2022ss/ss22-lookbook-women-08.jpg/ss22-lookbook-women-08.jpg/carhartt%3Alooksbigimage",
+        title:"Bips Star"
+    },
+    {
+        id: 4,
+        img:"https://www.carhartt-wip.com/binaries/content/gallery/pagetypes/looks/detail/men/2022ss/ss22-lookbook-men-01.jpg/ss22-lookbook-men-01.jpg/carhartt%3Alooksbigimage",
+        title:"Shorts Milkway"
+    },
+    {
+        id: 5,
+        img:"https://www.carhartt-wip.com/binaries/content/gallery/pagetypes/looks/detail/men/2022ss/ss22-lookbook-men-33.jpg/ss22-lookbook-men-33.jpg/carhartt%3Alookstripleimage",
+        title:"Shoes Sun"
+    }
+]
