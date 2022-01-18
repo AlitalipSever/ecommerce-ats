64700cc10cd4e60e13a36c0bb53e889daf097f74
AlitalipSever <alitalipsever@gmail.com>
2022-01-18 23:13:14 +0100

    Newsletter added

--- src/data.js
+++ src/data.js
@@ -1,14 +1,14 @@
 export const sliderItems = [
     {
         id:1,
-        img:"https://www.carhartt-wip.com/binaries/content/gallery/pagetypes/looks/detail/women/2022ss/ss22-lookbook-women-01.jpg/ss22-lookbook-women-01.jpg/carhartt%3Alooksbigimage",
+        img:"https://www.carhartt-wip.com/binaries/content/gallery/pagetypes/looks/detail/women/2022ss/ss22-lookbook-women-02.jpg/ss22-lookbook-women-02.jpg/carhartt%3Alooksbigimage",
         title: "Spring/Summer 2022 – Women",
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
         bg: "f5fafd",
     },
     {
         id:2,
-        img:"https://www.carhartt-wip.com/binaries/content/gallery/pagetypes/looks/detail/women/2022ss/ss22-lookbook-women-02.jpg/ss22-lookbook-women-02.jpg/carhartt%3Alooksbigimage",
+        img:"https://www.carhartt-wip.com/binaries/content/gallery/pagetypes/looks/detail/women/2022ss/ss22-lookbook-women-01.jpg/ss22-lookbook-women-01.jpg/carhartt%3Alooksbigimage",
         title: "Spring/Summer 2022 – Women",
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
         bg: "fcf1ed",
